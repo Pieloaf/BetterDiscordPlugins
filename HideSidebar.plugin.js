b9d2f415106e599839ff0d456d69d62c9d9b7fdd
Matthew Chan <accelyte@gmail.com>
2022-02-16 11:09:26 +0000

    fixes plugin not starting/working

--- HideSidebar.plugin.js
+++ HideSidebar.plugin.js
@@ -8,21 +8,24 @@
  * @source https://github.com/Pieloaf/BetterDiscordPlugins/blob/main/HideSidebar.plugin.js
  * @updateUrl https://raw.githubusercontent.com/Pieloaf/BetterDiscordPlugins/main/HideSidebar.plugin.js
  */
-module.exports = (_ => {
-
+module.exports = ((_) => {
     function createSelector(className) {
-        return '.' + className.replace(/ /g, '.')
+        return "." + className.replace(/ /g, ".");
     }
 
-    const sidebarSelector = createSelector(BdApi.findModuleByProps("hasNotice").sidebar)
-    const guildBarSelector = createSelector(BdApi.findModuleByProps("hasNotice").guilds)
-    const sidebarBtn = document.createElement('span');
+    const sidebarSelector = createSelector(
+        BdApi.findModuleByProps("guilds").sidebar
+    );
+    const guildBarSelector = createSelector(
+        BdApi.findModuleByProps("guilds").guilds
+    );
+    const sidebarBtn = document.createElement("span");
     const btnStyle = `
             .hide-sidebar-btn {
                 color: var(--interactive-normal);
                 background-color: var(--background-primary);
                 transition: background-color .15s ease-out,color .15s ease-out, border-radius .15s ease-out;
-                display: flex; 
+                display: flex;
                 justify-content: center;
                 margin: 6px 10px;
                 padding: 5px 3px;
@@ -37,42 +40,47 @@ module.exports = (_ => {
             }`;
 
     return class {
-
         start() {
-            BdApi.injectCSS('HideSidebarStyles', btnStyle)
-            this.initialise()
+            BdApi.injectCSS("HideSidebarStyles", btnStyle);
+            this.initialise();
         }
         stop() {
-            BdApi.clearCSS('HideSidebarStyles')
-            this.cleanup()
+            BdApi.clearCSS("HideSidebarStyles");
+            this.cleanup();
         }
 
         cleanup() {
-            sidebarBtn.remove()
+            sidebarBtn.remove();
         }
 
         createButton() {
-            const sidebar = document.querySelector(sidebarSelector)
+            const sidebar = document.querySelector(sidebarSelector);
 
-            sidebarBtn.innerHTML = '<<<'
-            sidebarBtn.classList.add('hide-sidebar-btn')
-            sidebarBtn.addEventListener('click', function () {
-                if (sidebar.style.display === "" || sidebar.style.display === "flex") {
-                    sidebar.style.display = "none"
-                    sidebarBtn.innerHTML = '>>>'
+            sidebarBtn.innerHTML = "<<<";
+            sidebarBtn.classList.add("hide-sidebar-btn");
+            sidebarBtn.addEventListener("click", function () {
+                if (
+                    sidebar.style.display === "" ||
+                    sidebar.style.display === "flex"
+                ) {
+                    sidebar.style.display = "none";
+                    sidebarBtn.innerHTML = ">>>";
+                } else {
+                    sidebar.style.display = "flex";
+                    sidebarBtn.innerHTML = "<<<";
                 }
-                else {
-                    sidebar.style.display = "flex"
-                    sidebarBtn.innerHTML = '<<<'
-                }
-            })
-            return sidebarBtn
+            });
+            return sidebarBtn;
         }
 
         observer(mutationRecord) {
-            const serverList = document.querySelector(guildBarSelector).firstChild.childNodes[1]
-            const serverListSelector = createSelector(serverList.classList.value)
-            if (mutationRecord.type !== 'childList') return;
+            const serverList =
+                document.querySelector(guildBarSelector).firstChild
+                    .childNodes[1];
+            const serverListSelector = createSelector(
+                serverList.classList.value
+            );
+            if (mutationRecord.type !== "childList") return;
             if (!mutationRecord.addedNodes.length) return;
             for (const node of Array.from(mutationRecord.addedNodes)) {
                 if (node.matches && node.matches(serverListSelector)) {
@@ -89,9 +97,15 @@ module.exports = (_ => {
         }
 
         initialise(serverListNode) {
-            this.createButton()
-            let serverList = serverListNode || document.querySelector(guildBarSelector).firstChild.childNodes[1]
-            serverList.insertBefore(sidebarBtn, serverList.firstElementChild.nextSibling);
+            this.createButton();
+            let serverList =
+                serverListNode ||
+                document.querySelector(guildBarSelector).firstChild
+                    .childNodes[1];
+            serverList.insertBefore(
+                sidebarBtn,
+                serverList.firstElementChild.nextSibling
+            );
         }
-    }
+    };
 })();
